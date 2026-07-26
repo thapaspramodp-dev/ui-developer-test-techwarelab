61692d304c93a41403dd7fca66ecbd176a2c04e3
thapaspramodp-dev <thapasppramod@gmail.com>
2026-07-26 21:29:17 +0530

    fix: dynamically set basePath only during production build for seamless localhost and GitHub Pages operation

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
-  basePath: '/ui-developer-test-techwarelab',
+  basePath: isProd ? '/ui-developer-test-techwarelab' : '',
   images: {
     unoptimized: true,
   },
